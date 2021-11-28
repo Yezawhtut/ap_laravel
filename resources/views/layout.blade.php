6834619cd0812bcfb4474ba8121bb7a7bb785b63
yezawhtut <mailbox.yezawhtut@gmail.com>
2021-11-28 07:11:44 +0630

    first commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,10 @@
+<html lang="en">
+<head>
+    <title>AP Laravel</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+</head>
+<h2 style="text-align:center">Simple Blog Site</h2> 
+<body>
+    @yield('content')
+</body>
+</html>
